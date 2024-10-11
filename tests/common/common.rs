e2a3067cf5dfe5509b11860b92e1158c57130769
pwh-pwh <plmoknpwh@163.com>
2024-10-11 22:31:53 +0800

    code fmt

--- tests/common/common.rs
+++ tests/common/common.rs
@@ -1,6 +1,6 @@
 use solana_client::rpc_client::RpcClient;
 
-pub fn get_rpc_client() ->RpcClient {
+pub fn get_rpc_client() -> RpcClient {
     let rpc_url = "https://api.mainnet-beta.solana.com";
     RpcClient::new(rpc_url)
 }
