588ac36ac3037564b9698eb7b9e6fa5947918208
pwh-pwh <plmoknpwh@163.com>
2024-10-16 20:59:36 +0800

    add some methods for get balance info

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
+use solana_transaction_status::option_serializer::OptionSerializer;
 use solana_transaction_status::{
-    EncodedConfirmedTransactionWithStatusMeta, EncodedTransaction, UiMessage,
+    EncodedConfirmedTransactionWithStatusMeta, EncodedTransaction, UiMessage
+    , UiTransactionTokenBalance,
 };
 
 pub mod utils;
 
 pub trait EncodedConfirmedTransactionMetaInfo {
     fn get_account_list(&self) -> Option<Vec<String>>;
+    fn get_pre_token_balance_info(&self) -> Option<Vec<UiTransactionTokenBalance>>;
+
+    fn get_post_token_balance_info(&self) -> Option<Vec<UiTransactionTokenBalance>>;
+
+    fn get_account_pre_balance(&self) -> Option<u64>;
+
+    fn get_account_post_balance(&self) -> Option<u64>;
 }
 
 impl EncodedConfirmedTransactionMetaInfo for EncodedConfirmedTransactionWithStatusMeta {
@@ -25,6 +34,40 @@ impl EncodedConfirmedTransactionMetaInfo for EncodedConfirmedTransactionWithStat
             _ => {
                 unimplemented!()
             }
+        }
+    }
+
+    fn get_pre_token_balance_info(&self) -> Option<Vec<UiTransactionTokenBalance>> {
+        match &self.transaction.meta {
+            None => None,
+            Some(meta) => match &meta.pre_token_balances {
+                OptionSerializer::Some(tb) => Some(tb.clone()),
+                _ => None,
+            },
+        }
+    }
+
+    fn get_post_token_balance_info(&self) -> Option<Vec<UiTransactionTokenBalance>> {
+        match &self.transaction.meta {
+            None => None,
+            Some(meta) => match &meta.post_token_balances {
+                OptionSerializer::Some(tb) => Some(tb.clone()),
+                _ => None,
+            },
+        }
+    }
+
+    fn get_account_pre_balance(&self) -> Option<u64> {
+        match &self.transaction.meta {
+            None => None,
+            Some(meta) => meta.pre_balances.first().copied(),
+        }
+    }
+
+    fn get_account_post_balance(&self) -> Option<u64> {
+        match &self.transaction.meta {
+            None => None,
+            Some(meta) => meta.post_balances.first().copied(),
         }
     }
 }
