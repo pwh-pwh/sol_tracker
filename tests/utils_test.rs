e987e2cde0b818108f12c16f6e365f5bd2a52abc
pwh-pwh <plmoknpwh@163.com>
2024-10-16 22:17:21 +0800

    add some test cases

--- tests/utils_test.rs
+++ tests/utils_test.rs
@@ -19,13 +19,24 @@ fn test_get_transaction_details() {
     let client = get_rpc_client();
     // let tx_id =
     //     "2YFvE4hLZXpbzaK43tyBnWAAk67Agq7pJ24AetDH6F4x8rLmMU6iTBHvUTWNAGdhHeuN5Bk61UL89U7Ks3id3SX";
+    //3xQpd3xSthQ1S589shuqpaVmgBoug9eu4g1g6mi9yJF4QjF1WrSHKPG3KrBUYDcre7WM6rJqqXKbKTy1ivLX9ZX3
+    //2FM9dAMegHqt75rapiD5Ynpbgd4kT2t7g4QPXhhWLxCXCoGgxgSZwGLDYt3QHXZqDcmNivK7YJ89FXLrS42m2JNS
     let tx_id =
-        "2FM9dAMegHqt75rapiD5Ynpbgd4kT2t7g4QPXhhWLxCXCoGgxgSZwGLDYt3QHXZqDcmNivK7YJ89FXLrS42m2JNS";
+        "5uWAsNnrLaUudAqYDDm4qLBsLCgPy9pBYEStntdfaqSTVfnDirQFE3QzQXhUr63gaypK66BX694pwkub6Zfv68HM";
     let meta = utils::get_transaction_details(&client, tx_id).unwrap();
     // dbg!(&meta);
     dbg!(meta.get_account_list());
+    dbg!(meta.get_pre_token_balance_info());
+    dbg!(meta.get_post_token_balance_info());
+    let pret = meta.get_pre_token_balance_info();
+    let postt = meta.get_post_token_balance_info();
+    let info = utils::calculate_transaction_info(
+        &pret.unwrap(),
+        &postt.unwrap(),
+        "CANTSsRNWR2ykW4YejwMPgLBJ1GbR1FYoX47yJconumj",
+    );
+    dbg!(info);
 }
-
 
 #[test]
 fn test_get_lastest_transaction_signature() {
